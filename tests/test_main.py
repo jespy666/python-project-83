940e4ed89302903745a0b95ea92f9e0409c8b033
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2023-09-13 17:09:10 +0600

    expand tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,37 @@
 import pytest
 from page_analyzer import app
+from page_analyzer.psql_db import execute_sql_script
 
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    client = app.test_client()
-    yield client
+    with app.test_client() as client:
+        with app.app_context():
+            execute_sql_script()
+        yield client
+    with app.app_context():
+        execute_sql_script()
 
 
-def test_main_page(client):
+def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
     assert 'Анализатор страниц' in response.data.decode('utf-8')
     assert 'Сайты' in response.data.decode('utf-8')
+
+
+def test_valid_url(client):
+    response = client.post('/urls', data={'url': 'https://ru.hexlet.io/'})
+    assert response.status_code == 302
+    assert '/urls/1' in response.headers['Location']
+    with client.session_transaction() as session:
+        flash_messages = session['_flashes']
+        assert flash_messages
+        assert 'Страница успешно добавлена' in flash_messages[0]
+
+
+def test_invalid_url(client):
+    response = client.post('/urls', data={'url': 'wrong://url'})
+    assert response.status_code == 422
+
