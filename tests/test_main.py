83429fc5506c8171b686b5f56b7de5c3590d0430
Anton Georgievsky <123451311+jespy666@users.noreply.github.com>
2023-09-14 14:07:38 +0600

    add check tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,16 +21,36 @@ def test_home_page(client):
     assert 'Сайты' in response.data.decode('utf-8')
 
 
-def test_valid_url(client):
-    response = client.post('/urls', data={'url': 'https://ru.hexlet.io/'})
+@pytest.mark.parametrize("url, expected_message", [
+    ('https://ru.hexlet.io/', 'Страница успешно добавлена'),
+    ('wrong://url', None)
+])
+def test_url_submission(client, url, expected_message):
+    response = client.post('/urls', data={'url': url})
+
+    if expected_message:
+        assert response.status_code == 302
+        assert '/urls/1' in response.headers['Location']
+
+        with client.session_transaction() as session:
+            flash_messages = session['_flashes']
+            assert flash_messages
+            assert next(
+                (i for i in flash_messages if expected_message in i[1]), None)
+    else:
+        assert response.status_code == 422
+
+
+@pytest.mark.parametrize("url, expected_message", [
+    ('https://ru.hexlet.io/', 'Страница успешно проверена'),
+    ('https://wrongurlwrong.com', 'Произошла ошибка при проверке')
+])
+def test_check_url(client, url, expected_message):
+    client.post('/urls', data={'url': url})
+    response = client.post('/urls/1/checks')
     assert response.status_code == 302
-    assert '/urls/1' in response.headers['Location']
     with client.session_transaction() as session:
         flash_messages = session['_flashes']
         assert flash_messages
-        assert 'Страница успешно добавлена' in flash_messages[0]
-
-
-def test_invalid_url(client):
-    response = client.post('/urls', data={'url': 'wrong://url'})
-    assert response.status_code == 422
+        assert next(
+            (i for i in flash_messages if expected_message in i[1]), None)
